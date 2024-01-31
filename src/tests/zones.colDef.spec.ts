3f03b1c9ca2178b799463f2fa99ac8e5b5cb8b9d
Stephen Cooper <scooperdev@gmail.com>
2024-01-31 10:33:00 +0000

    AG-10552 Add context menu action test

--- src/tests/zones.colDef.spec.ts
+++ src/tests/zones.colDef.spec.ts
@@ -5,19 +5,21 @@ import userEvent from '@testing-library/user-event';
 
 import { AgGridAngular } from '@ag-grid-community/angular';
 import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
-import { CellClickedEvent, CellDoubleClickedEvent, ColDef, Module, NewValueParams } from '@ag-grid-community/core';
+import { CellClickedEvent, CellDoubleClickedEvent, ColDef, GridOptions, Module, NewValueParams } from '@ag-grid-community/core';
+import { MenuModule } from '@ag-grid-enterprise/menu';
 
 @Component({
     selector: 'app-grid-wrapper',
     standalone: true,
     imports: [AgGridAngular],
     template: `<ag-grid-angular
+        [gridOptions]="gridOptions"
         [columnDefs]="columnDefs"
         [rowData]="rowData"
         [modules]="modules"></ag-grid-angular>`,
 })
 export class GridWrapperComponent {
-    modules: Module[] = [ClientSideRowModelModule];
+    modules: Module[] = [ClientSideRowModelModule, MenuModule];
     rowData: any[] = [{ make: 'Toyota', model: 'Celica', price: 35000 }];
     columnDefs: ColDef[] = [
         {
@@ -41,11 +43,36 @@ export class GridWrapperComponent {
         { field: 'price' },
     ];
 
+    gridOptions: GridOptions = {
+        getContextMenuItems: (params) => {
+            return [
+                {
+                    name: 'Custom Menu Item',
+                    action: () => {
+                        this.zoneStatus['customMenuItem'] = NgZone.isInAngularZone();
+                    },
+                },
+            ];
+        }
+    };
+
     public zoneStatus: { [key: string]: boolean } = {};
 
 }
 
 describe('Test ColDef Event ZoneJs Status', () => {
+
+    // suppress console errors
+    let originalError: any;
+    beforeAll(() =>{
+        originalError = console.error;
+        spyOn(console, 'error');        
+    })
+    afterAll(() =>{
+        console.error = originalError;
+    });
+
+
     it('Test cell is rendered and price updated', async () => {
         await render(GridWrapperComponent);
         const toyota = await screen.findByText('Toyota');
@@ -70,6 +97,9 @@ describe('Test ColDef Event ZoneJs Status', () => {
 
         // Validate context menu
         await user.pointer({keys: '[MouseRight]', target: toyota});
+        const customMenuItem = await screen.findByText('Custom Menu Item');
+
+        await user.click(customMenuItem);
 
         expect(component.fixture.componentInstance.zoneStatus['cellClicked']).toBeTrue();
         expect(component.fixture.componentInstance.zoneStatus['cellDoubleClicked']).toBeTrue();
